67c687a87e1a71127787d244670a61baee255316
FeatheredSnek <dorian.maczka@gmail.com>
2021-08-13 23:25:20 +0200

    Sketch website functionality and styling

--- sketch.js
+++ sketch.js
@@ -1,9 +1,66 @@
-// rotator for rotating
 let rotator = 0
-let plant
+let plant, canvasElement
+let modalShown = false
+
+const modalWrapper = document.getElementById('modal')
+const modalContent = document.getElementById('modal-content')
+const inputField = document.getElementById('code-input')
+
+function loadRandomPlant () {
+  noLoop()
+  plant = new Plant()
+  loop()
+}
+
+function loadPlantFromCode () {
+  let inputValidated
+  if (!inputField.value) {
+    showModalMessage('Enter plant code', 2)
+  }
+  else {
+    inputValidated = parser.parseInput(inputField.value)
+    if (!inputValidated)
+    {
+      showModalMessage('Invalid code', 2)
+    }
+    else {
+      noLoop()
+      plant = new Plant(inputValidated)
+      loop()
+      showModalMessage('Plant loaded', 2)
+    }
+    inputField.value = ''
+  }
+}
+
+function copyPlantToClipboard () {
+  const code = parser.plantToCode(plant.log())
+  const successMessage = 'Plant code copied to clipboard'
+  const failureMessage = 'Cannot access clipboard, use the console (F12)'
+  navigator.clipboard.writeText(code)
+    .then(showModalMessage(successMessage, 2))
+    .catch(() => {
+      showModalMessage(failureMessage, 3)
+      console.log('Your plant code:')
+      console.log(code)
+    })
+}
+
+function showModalMessage (message, time) {
+  if (!modalShown) {
+    modalShown = true
+    modalWrapper.classList.remove('modal-disabled')
+    modalContent.textContent = message
+    setTimeout(() => {
+      modalWrapper.classList.add('modal-disabled')
+      modalShown = false
+    }, time * 1000)
+  }
+}
 
 function setup() {
-  createCanvas(400, 400, WEBGL)
+  canvasElement = createCanvas(400, 400, WEBGL)
+  canvasElement.parent('canvas-wrapper')
   frameRate(30)
   plant = new Plant()
 }
@@ -14,6 +71,5 @@ function draw() {
   rotateX(PI - 0.4)
   rotateY(rotator)
   rotator += 0.01
-
   plant.draw()
 }
